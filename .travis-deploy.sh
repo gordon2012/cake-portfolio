3247983ee429b6d01c9192d800ee815272a19c44
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-06-08 23:21:37 -0700

    Fix travis deploy

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,11 +1,15 @@
 date
-cd dist
+
+rm -rf .gitignore
+rm -rf .git
+rm -rf config/app.php
+
 git init
 
 git config user.name "Travis CI"
 git config user.email "gordon2012@users.noreply.github.com"
 
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy to server"
 
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:deploy > /dev/null 2>&1
